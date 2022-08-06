6267e836c2e4f37a33998291e3ac7fb410b3498d
Manuel Carrasco <manuelecarrasco96@gmail.com>
2022-08-06 00:01:50 -0300

    Creacion de tarjetas de productos en seccion merch

--- js/main.js
+++ js/main.js
@@ -4,18 +4,59 @@ const productos = [],
   carrito = [];
 
 class Ropa {
-  constructor(nombre, precio) {
+  constructor(nombre, precio, imagen, id) {
     this.nombre = nombre;
     this.precio = precio;
+    this.imagen = imagen;
+    this.id = id;
   }
 }
 
-productos.push(new Ropa("Remera Negra", 29.99));
-productos.push(new Ropa("Sweater Negro", 39.99));
-productos.push(new Ropa("Remera Blanca", 34.99));
-productos.push(new Ropa("Sweater Gris", 44.99));
-productos.push(new Ropa("Remera Gris", 24.99));
+productos.push(
+  new Ropa("Black T-Shirt", 29.99, "../images/black-t-shirt.jpg", 1)
+);
+productos.push(
+  new Ropa("Black Sweater", 39.99, "../images/black-sweater.jpg", 2)
+);
+productos.push(
+  new Ropa("White T-Shirt", 34.99, "../images/white-t-shirt.jpg", 3)
+);
+productos.push(
+  new Ropa("Gray Sweater", 44.99, "../images/gray-sweater.jpg", 4)
+);
+productos.push(
+  new Ropa("Gray T-Shirt", 24.99, "../images/gray-t-shirt.jpg", 5)
+);
 
+const contenedorProductos = document.getElementById("contenedor-productos");
+
+productos.forEach((producto) => {
+  const div = document.createElement("div");
+  div.classList.add("div__merch", "mb-5");
+  div.innerHTML = `
+        <!--Div contenedor de la ropa, descripcion y precio-->
+        <div>
+          <!--Div contenedor de la imagen-->
+          <img src=${producto.imagen} alt="Black T-shirt">
+        </div>
+        <div>
+          <!--Div contenedor de la descripcion y precio-->
+          <p><span>$ ${producto.precio}</span></p>
+          <!--Precio-->
+          <div>
+            <!--Descripcion-->
+            <p>${producto.nombre}</p>
+            <!--Descripcion-->
+          </div>
+        </div>
+        <div class="mb-3 mt-4 d-flex justify-content-center">
+          <!--Div de boton-->
+          <button type="button" class="btn">Buy</button>
+        </div>
+  `;
+
+  contenedorProductos.appendChild(div);
+});
 //Funcion que quita los espacios en blanco
 function quitarEspacios(text) {
   let regEx = new RegExp(" ", "g");
