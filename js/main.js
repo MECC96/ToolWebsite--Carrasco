56eefc2e0960fa00454e89da37241d3b5ba1f701
Manuel Carrasco <manuelecarrasco96@gmail.com>
2022-08-06 22:36:23 -0300

    se agrego evento a boton buy

--- js/main.js
+++ js/main.js
@@ -49,102 +49,109 @@ productos.forEach((producto) => {
             <!--Descripcion-->
           </div>
         </div>
-        <div class="mb-3 mt-4 d-flex justify-content-center">
-          <!--Div de boton-->
-          <button type="button" class="btn">Buy</button>
-        </div>
+          <button id="btn${producto.id}" class="btn">Buy</button>
   `;
-
   contenedorProductos.appendChild(div);
-});
-//Funcion que quita los espacios en blanco
-function quitarEspacios(text) {
-  let regEx = new RegExp(" ", "g");
-  return text.replace(regEx, "");
-}
-//funcion que recorre el array productos para ver si existe el articulo a comprar
-function encontrarRopa(arrayRopa, ropaAEncontrar) {
-  for (ropa of arrayRopa) {
-    if (
-      quitarEspacios(ropaAEncontrar).toLowerCase() ==
-      quitarEspacios(ropa.nombre).toLowerCase()
-    )
-      return ropa;
-  }
-}
-
-//funcion que recorre el array productos para retornar el indice donde se encuentra el producto a comprar
-function encontrarIndice(arrayRopa, ropaAEncontrar) {
-  for (ropa of arrayRopa) {
-    if (
-      quitarEspacios(ropaAEncontrar).toLowerCase() ==
-      quitarEspacios(ropa.nombre).toLowerCase()
-    )
-      return arrayRopa.indexOf(ropa);
-  }
-}
-
-//funcion que pide al usuario la cantidad de unidades que quiere del producto
-function pedirCantidadArticulo(numero) {
-  let unidades = parseInt(
-    prompt(`Ingrese la cantidad de unidades que desea del artículo ${numero}:`)
-  );
-  if (unidades > 0) {
-    return unidades;
-  } else {
-    alert("Dato ingresado inválido. Por favor, intente nuevamente.");
-    pedirCantidadArticulo(numero);
-  }
-}
-
-//funcion para imprimir los articulos comprados
-function imprimirTicket() {
-  carrito.forEach((carritoFinal) => {
-    console.log(
-      `producto: ${carritoFinal.artículo}, unidades: ${
-        carritoFinal.cantidad
-      }, total por producto: $${carritoFinal.cantidad * carritoFinal.monto}`
-    );
+  const boton = document.getElementById(`btn${producto.id}`);
+  boton.addEventListener("click", () => {
+    agregarAlCarrito(producto.id);
   });
-  const total = carrito.reduce(
-    (acc, el) => acc + parseFloat(el.monto) * parseInt(el.cantidad),
-    0
-  );
-  console.log(`El total a pagar por su compra es: $${total}`);
-  alert("Gracias por tu compra! Hasta pronto");
+});
+//Funcion que agrega elementos al carrito
+function agregarAlCarrito(prodId) {
+  const item = productos.find((prod) => prod.id === prodId);
+  carrito.push(item);
+  console.log(carrito);
 }
 
-function ingresarArtículo() {
-  artículo = prompt(`Escriba el nombre del artículo que desee comprar:
-    1) Remera Negra $29.99.
-    2) Sweater Negro $39.99.
-    3) Remera Blanca $34.99.
-    4) Sweater Gris $44.99.
-    5) Remera Gris $24.99.`);
+// //Funcion que quita los espacios en blanco
+// function quitarEspacios(text) {
+//   let regEx = new RegExp(" ", "g");
+//   return text.replace(regEx, "");
+// }
+// //funcion que recorre el array productos para ver si existe el articulo a comprar
+// function encontrarRopa(arrayRopa, ropaAEncontrar) {
+//   for (ropa of arrayRopa) {
+//     if (
+//       quitarEspacios(ropaAEncontrar).toLowerCase() ==
+//       quitarEspacios(ropa.nombre).toLowerCase()
+//     )
+//       return ropa;
+//   }
+// }
 
-  let ropaEncontrada = encontrarRopa(productos, artículo);
+// //funcion que recorre el array productos para retornar el indice donde se encuentra el producto a comprar
+// function encontrarIndice(arrayRopa, ropaAEncontrar) {
+//   for (ropa of arrayRopa) {
+//     if (
+//       quitarEspacios(ropaAEncontrar).toLowerCase() ==
+//       quitarEspacios(ropa.nombre).toLowerCase()
+//     )
+//       return arrayRopa.indexOf(ropa);
+//   }
+// }
 
-  if (ropaEncontrada) {
-    let indice = encontrarIndice(productos, artículo);
+// //funcion que pide al usuario la cantidad de unidades que quiere del producto
+// function pedirCantidadArticulo(numero) {
+//   let unidades = parseInt(
+//     prompt(`Ingrese la cantidad de unidades que desea del artículo ${numero}:`)
+//   );
+//   if (unidades > 0) {
+//     return unidades;
+//   } else {
+//     alert("Dato ingresado inválido. Por favor, intente nuevamente.");
+//     pedirCantidadArticulo(numero);
+//   }
+// }
 
-    cantidad = parseInt(pedirCantidadArticulo(indice + 1));
-    let monto = productos[indice].precio;
-    artículo = productos[indice].nombre;
-    carrito.push({ artículo, cantidad, monto });
-    answer = confirm(`¿Deseas hacer otra compra?`);
-  } else {
-    alert("El producto no existe");
-    ingresarArtículo();
-  }
-}
+// //funcion para imprimir los articulos comprados
+// function imprimirTicket() {
+//   carrito.forEach((carritoFinal) => {
+//     console.log(
+//       `producto: ${carritoFinal.artículo}, unidades: ${
+//         carritoFinal.cantidad
+//       }, total por producto: $${carritoFinal.cantidad * carritoFinal.monto}`
+//     );
+//   });
+//   const total = carrito.reduce(
+//     (acc, el) => acc + parseFloat(el.monto) * parseInt(el.cantidad),
+//     0
+//   );
+//   console.log(`El total a pagar por su compra es: $${total}`);
+//   alert("Gracias por tu compra! Hasta pronto");
+// }
 
-//Mensaje de bienvenida al usuario
-alert(`¡Bienvenidos a la tienda virtual de la banda TOOL!`);
+// function ingresarArtículo() {
+//   artículo = prompt(`Escriba el nombre del artículo que desee comprar:
+//     1) Remera Negra $29.99.
+//     2) Sweater Negro $39.99.
+//     3) Remera Blanca $34.99.
+//     4) Sweater Gris $44.99.
+//     5) Remera Gris $24.99.`);
 
-//ciclo que ejecuta el algoritmo las veces que el usuario desee
+//   let ropaEncontrada = encontrarRopa(productos, artículo);
 
-do {
-  ingresarArtículo();
-} while (answer);
-//Funncion que muestra los productos, unidades, monto por cada articulo y monto total de la compra por consola
-imprimirTicket();
+//   if (ropaEncontrada) {
+//     let indice = encontrarIndice(productos, artículo);
+
+//     cantidad = parseInt(pedirCantidadArticulo(indice + 1));
+//     let monto = productos[indice].precio;
+//     artículo = productos[indice].nombre;
+//     carrito.push({ artículo, cantidad, monto, id });
+//     answer = confirm(`¿Deseas hacer otra compra?`);
+//   } else {
+//     alert("El producto no existe");
+//     ingresarArtículo();
+//   }
+// }
+
+// //Mensaje de bienvenida al usuario
+// alert(`¡Bienvenidos a la tienda virtual de la banda TOOL!`);
+
+// //ciclo que ejecuta el algoritmo las veces que el usuario desee
+
+// do {
+//   ingresarArtículo();
+// } while (answer);
+// //Funncion que muestra los productos, unidades, monto por cada articulo y monto total de la compra por consola
+// imprimirTicket();
